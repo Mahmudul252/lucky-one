ab8cd17687f4dcf7a933196926522de66cfb8d40
Mahmudul252 <mahmudulkabir990@gmail.com>
2022-03-26 01:36:42 -0500

    header added

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="heading">Puzzled Buyer</h1>
+            <h3>Choose 4 Sunglass</h3>
+        </div>
+    );
+};
+
+export default Header;
